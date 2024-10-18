23908bdb4118d7f943883196f0fc4835466034c5
Varvara <varvarka.rudskaia@gmail.com>
2024-10-18 21:20:49 +0300

    first commit

---
+++ app/src/main/java/com/example/buttontest/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.buttontest
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val hello = findViewById<Button>(R.id.hello)
+        val textView =  findViewById<TextView>(R.id.textView)
+        hello.setOnClickListener {
+            textView.visibility = View.VISIBLE
+        }
+    }
+}
